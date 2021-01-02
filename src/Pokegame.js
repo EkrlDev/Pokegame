6519e8dd174322118e6126f8db3ce473f3307c52
ErhnKrl <ye.koral@gmail.com>
2021-01-02 05:06:36 +0300

    clear interval and select added

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -17,7 +17,9 @@ class Pokegame extends Component {
             exp1: 0,
             exp2: 0,
             account: 500,
-            rekt: false
+            rekt: false,
+            rolling: false,
+            time: 1
         }
     }
 
@@ -36,38 +38,46 @@ class Pokegame extends Component {
 
     setBalance = () => {
 
-    
-            setInterval(() => {
-            
+            this.setState({rolling: true});
+
+            let times = 0;
                 
+            const myInterval = setInterval(() => {
+                
+                    
                     const hand1 = [];
-                const hand2 = [...this.props.pokemon];
-                let account = this.state.account;
-        
-            while (hand1.length < hand2.length) {
-                let randidx = Math.floor(Math.random() * hand2.length);
-                let randPokemon = hand2.splice(randidx, 1)[0];
-                hand1.push(randPokemon);
-            }
-        
-            const exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
-            const exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+                    const hand2 = [...this.props.pokemon];
+                    let account = this.state.account;
             
-            if(exp1 > exp2) {
-                account--;
-            } else {
-                account++;
-            }
-
-            
-            this.setState({account: account, exp1: exp1, exp2: exp2, hand1:hand1, hand2: hand2});
+                while (hand1.length < hand2.length) {
+                    let randidx = Math.floor(Math.random() * hand2.length);
+                    let randPokemon = hand2.splice(randidx, 1)[0];
+                    hand1.push(randPokemon);
                 }
+            
+                const exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+                const exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+                
+                if(exp1 > exp2) {
+                    account--;
+                } else {
+                    account++;
+                }
+    
+                
+                this.setState({account: account, exp1: exp1, exp2: exp2, hand1:hand1, hand2: hand2});
+                times++;
+                console.log(times);
 
-            , 1000);
-        
-        
-        
+                if(times === this.state.time) {
+                    clearInterval(myInterval);
+                    this.setState({rolling: false});
+                }
+            }, 1000);
+
     }
+
+    
 
     render() {
         
@@ -76,7 +86,13 @@ class Pokegame extends Component {
             <div>
                 <Pokedex pokemon = {this.state.hand1} exp = {this.state.exp1} isWinner = {this.state.exp2 < this.state.exp1} who="PEPE"/>
                 <Pokedex pokemon = {this.state.hand2} exp = {this.state.exp2} isWinner = {this.state.exp2 > this.state.exp1} who="YOU"/>
-                <button className="Pokedex-button" onClick={this.setBalance}>Roll</button>
+                <select className="Pokedex-button" onChange={(e) => this.setState({time: parseInt(e.target.value)})}>
+                    <option value="1">1</option>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="50">50</option>
+                </select>
+                <button className="Pokedex-button" onClick={this.setBalance} disabled={this.state.rolling}>{this.state.rolling ? 'Rolling!' : 'Roll'}</button>
                 <p className="Pokedex">Your Balance: {this.state.account}</p>
                 
             </div>
